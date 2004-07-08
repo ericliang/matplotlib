cbc28776ff6cc7ecdcc8c01733c64b12d965e998
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2004-07-08 16:57:38 +0000

    some numarray workarounds
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@385 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/htdocs/examples/dynamic_demo_wx.py
+++ trunk/htdocs/examples/dynamic_demo_wx.py
@@ -20,8 +20,8 @@ embedding_in_wx.py, which provides these features.
 
 Modification History:
 $Log$
-Revision 1.43  2004/07/07 15:56:19  jdh2358
-sync to 0.60
+Revision 1.44  2004/07/08 16:57:38  jdh2358
+some numarray workarounds
 
 Revision 1.5  2004/06/26 06:37:20  astraw
 Trivial bugfix to eliminate IndexError
