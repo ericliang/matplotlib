11f860677c8589cbffd0ec242808c77bf9086874
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2004-07-15 19:41:19 +0000

    fixed tk framework for os x
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@403 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/htdocs/examples/dynamic_demo_wx.py
+++ trunk/htdocs/examples/dynamic_demo_wx.py
@@ -20,8 +20,8 @@ embedding_in_wx.py, which provides these features.
 
 Modification History:
 $Log$
-Revision 1.46  2004/07/10 20:01:07  jdh2358
-moved rc to matplotlib
+Revision 1.47  2004/07/15 19:41:19  jdh2358
+fixed tk framework for os x
 
 Revision 1.5  2004/06/26 06:37:20  astraw
 Trivial bugfix to eliminate IndexError
