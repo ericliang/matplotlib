66c7533b65af7971bb17def5f87850f0eefeba95
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2004-08-16 21:20:55 +0000

    updated datetime docstring
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@499 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/htdocs/examples/dynamic_demo_wx.py
+++ trunk/htdocs/examples/dynamic_demo_wx.py
@@ -20,8 +20,8 @@ embedding_in_wx.py, which provides these features.
 
 Modification History:
 $Log$
-Revision 1.57  2004/08/15 17:27:03  jdh2358
-fixed cursor demos
+Revision 1.58  2004/08/16 21:20:54  jdh2358
+updated datetime docstring
 
 Revision 1.5  2004/06/26 06:37:20  astraw
 Trivial bugfix to eliminate IndexError
