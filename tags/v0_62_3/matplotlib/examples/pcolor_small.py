7caae50c25c61f6d65d307f4986314b260be426f
(no author) <(no author)@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2004-08-30 17:40:35 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_62_3'.
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@521 f61c4167-ca0d-0410-bb4a-bb21726e55ed

---
+++ tags/v0_62_3/matplotlib/examples/pcolor_small.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from matplotlib.matlab import *
+
+#Z = arange(60)
+#Z.shape = 6,10
+#Z.shape = 10,6
+#print Z
+Z = rand(10,6)
+
+#c = pcolor(Z, shading='flat') # default 'faceted'
+c = pcolor(Z)
+c.set_linewidth(4)
+
+#savefig('pcolor_small')
+show()
