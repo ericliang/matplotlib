1f4dea386b12b831a6fe8db546bcde5a5cc2d1fb
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2004-08-30 18:52:59 +0000

    fixed stoopid mathtext bug
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@522 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/htdocs/examples/dynamic_demo_wx.py
+++ trunk/htdocs/examples/dynamic_demo_wx.py
@@ -20,8 +20,8 @@ embedding_in_wx.py, which provides these features.
 
 Modification History:
 $Log$
-Revision 1.61  2004/08/28 23:44:03  jdh2358
-sync to 0.62.0
+Revision 1.62  2004/08/30 18:52:58  jdh2358
+fixed stoopid mathtext bug
 
 Revision 1.5  2004/06/26 06:37:20  astraw
 Trivial bugfix to eliminate IndexError
