8ba9cc49520339e24ccbeac1f5e951e10afabae4
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2005-02-08 20:07:03 +0000

    released basemap
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@940 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/htdocs/examples/dynamic_demo_wx.py
+++ trunk/htdocs/examples/dynamic_demo_wx.py
@@ -20,8 +20,8 @@ embedding_in_wx.py, which provides these features.
 
 Modification History:
 $Log$
-Revision 1.86  2005/01/21 22:34:32  jdh2358
-syncing to 0.71
+Revision 1.87  2005/02/08 20:06:33  jdh2358
+released basemap
 
 Revision 1.6  2004/10/26 18:08:13  astraw
 Converted to use new NavigationToolbar2 (from old Toolbar).
