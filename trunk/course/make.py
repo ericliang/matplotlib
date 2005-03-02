b691e683affa199cc2029f53316723f5d2fbe0c0
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2005-03-02 16:10:30 +0000

    added make
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@1001 f61c4167-ca0d-0410-bb4a-bb21726e55ed

---
+++ trunk/course/make.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os, sys
+
+def clean():
+    print 'cleaning...'
+    os.system('rm -f *.bbl *.blg *.dvi *.log *.toc *.aux *.tex *~ #*')
+
+for arg in sys.argv[1:]:
+    if arg=='clean': clean()
+    elif arg=='whatever': pass  # add your funcs here
+    else: raise ValueError('Unrecognized command "%s"' % arg)
