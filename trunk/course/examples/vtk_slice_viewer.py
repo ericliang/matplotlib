29d3595494983af536c88615d9620e531148c1eb
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2005-03-02 20:57:28 +0000

    added slicer
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@1007 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/course/examples/vtk_slice_viewer.py
+++ trunk/course/examples/vtk_slice_viewer.py
@@ -1,10 +1,11 @@
 #!/usr/local/bin/python
 import os
-from vtk import vtkVolume16Reader, vtkImageViewer
-from WindowLevelInterface import *
+import vtk
+from WindowLevelInterface import WindowLevelInterface
 
-# create reader
-reader = vtkVolume16Reader()
+# Create reader - you have total flexibility to specify the file
+# naming pattern, the byte order, the size of the header and so on
+reader = vtk.vtkVolume16Reader()
 reader.SetDataDimensions(256,256)
 reader.GetOutput().SetOrigin(0.0,0.0,0.0)
 reader.SetFilePrefix('../data/images/r')
@@ -14,16 +15,16 @@ reader.SetImageRange(1001,1060)
 reader.SetDataSpacing(1.0,1.0,3.5)
 reader.Update()
 
-viewer = vtkImageViewer()
+# VTK comes with a helper class to view slice data
+viewer = vtk.vtkImageViewer()
 viewer.SetInput(reader.GetOutput())
 viewer.SetZSlice(30)
 viewer.SetColorWindow(600)
 viewer.SetColorLevel(270)
 viewer.Render()
-
 viewer.SetPosition(50,50)
 
-#make interface
+# A helper class to set the window level, etc
 WindowLevelInterface(viewer)
 
 
