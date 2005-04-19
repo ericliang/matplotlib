7f18a966dd580afe2b892f5dff5571a61f7dfe21
jswhit <jswhit@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2005-04-19 11:24:30 +0000

    code cleanup.
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@1220 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/htdocs/screenshots/plotmap.py
+++ trunk/htdocs/screenshots/plotmap.py
@@ -20,11 +20,13 @@ m = Basemap(-145.5,1.,-2.566,46.352,\
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
+# set up figure with same aspect ratio as map.
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
+# plot image over map.
 im = m.imshow(topodat,cm.jet)
-cax = axes([0.875, 0.1, 0.05, 0.7])
+cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes
 colorbar(tickfmt='%d', cax=cax) # draw colorbar
 axes(ax)  # make the original axes current again
 m.drawcoastlines()
@@ -35,11 +37,11 @@ m.drawstates()
 delat = 20.
 parallels = arange(0.,90.+delat,delat).tolist()+\
           arange(-delat,-90.-delat,-delat).tolist()
-m.drawparallels(parallels,labels=[1,1,0,1])
+m.drawparallels(parallels,labels=[1,1,0,1]) # labels on left, right, bottom
 # draw meridians
 delon = 30.
 meridians = arange(10.,360.,delon)
-m.drawmeridians(meridians,labels=[1,1,0,1])
+m.drawmeridians(meridians,labels=[1,1,0,1]) # labels on left, right, bottom.
 
 title('ETOPO Topography - Lambert Conformal Conic')
 show()
