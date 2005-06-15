e04d42101f33e0366b944f1994c92d3dc498b018
jdh2358 <jdh2358@f61c4167-ca0d-0410-bb4a-bb21726e55ed>
2005-06-15 20:23:17 +0000

    sync to 0.82
    git-svn-id: https://matplotlib.svn.sourceforge.net/svnroot/matplotlib@1470 f61c4167-ca0d-0410-bb4a-bb21726e55ed

--- trunk/htdocs/screenshots/slider_demo.py
+++ trunk/htdocs/screenshots/slider_demo.py
@@ -1,19 +1,21 @@
 from pylab import *
-from matplotlib.widgets import Slider, Button
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 ax = subplot(111)
-subplots_adjust(bottom=0.25)
+subplots_adjust(left=0.25, bottom=0.25)
 t = arange(0.0, 1.0, 0.001)
-s = sin(2*pi*t)
-l, = plot(t,s, lw=2)
+a0 = 5
+f0 = 3
+s = a0*sin(2*pi*f0*t)
+l, = plot(t,s, lw=2, color='red')
 axis([0, 1, -10, 10])
 
 axcolor = 'lightgoldenrodyellow'
-axfreq = axes([0.125, 0.1, 0.775, 0.03], axisbg=axcolor)
-axamp  = axes([0.125, 0.15, 0.775, 0.03], axisbg=axcolor)
+axfreq = axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+axamp  = axes([0.25, 0.15, 0.65, 0.03], axisbg=axcolor)
 
-sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=1)
-samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=1)
+sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
+samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
 
 def update(val):
     amp = samp.val
@@ -24,13 +26,18 @@ sfreq.on_changed(update)
 samp.on_changed(update)
 
 resetax = axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset')
-
+button = Button(resetax, 'Reset', color=axcolor, hovercolor=0.975)
 def reset(event):
     sfreq.reset()
     samp.reset()
 button.on_clicked(reset)
-    
+
+rax = axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
+radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
+def colorfunc(label):
+    l.set_color(label)
+    draw()
+radio.on_clicked(colorfunc)
 
 show()
 
